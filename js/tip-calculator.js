0f80db975d044d718ed46a1610bad62ceb65ef85
AnotherCreator <jreginaldo@protonmail.com>
2023-10-29 13:58:26 -0500

    Add error handling for negative and non number values

--- js/tip-calculator.js
+++ js/tip-calculator.js
@@ -1,15 +1,25 @@
 function tipCalculator() {
     // Variables
     // Explicitly convert string types to nums
-    let subTotal = parseFloat(document.getElementById("billTotal").value);
-    let tipPercentage = parseInt(document.getElementById("tipSlider").value);
+    let subTotal = document.getElementById("billTotal").value;
+    let tipPercentage = document.getElementById("tipSlider").value;
 
     // Calculate
-    let tipAmount = subTotal * tipPercentage * .01
-    let finalTotal = subTotal + tipAmount
+    if (subTotal < 0) { // Handle negative values
+        document.getElementById("tipPercentage").value = `Error: Negative Value`;
+        document.getElementById("tipAmount").value = `Error: Negative Value`;
+        document.getElementById("finalTotal").value = `Error: Negative Value`;
+    } else if (isNaN(subTotal)) { // Handle non-numerical values
+        document.getElementById("tipPercentage").value = `Error: Non-Numerical Value`;
+        document.getElementById("tipAmount").value = `Error: Non-Numerical Value`;
+        document.getElementById("finalTotal").value = `Error: Non-Numerical Value`;
+    } else {
+        let tipAmount = parseFloat(subTotal) * parseInt(tipPercentage) * .01
+        let finalTotal = parseFloat(subTotal) + tipAmount
 
-    // Display values
-    document.getElementById("tipPercentage").value = `${tipPercentage} %`;
-    document.getElementById("tipAmount").value = `$ ${tipAmount.toFixed(2)}`;
-    document.getElementById("finalTotal").value = `$ ${finalTotal.toFixed(2)}`;
+        // Display values
+        document.getElementById("tipPercentage").value = `${tipPercentage} %`;
+        document.getElementById("tipAmount").value = `$ ${tipAmount.toFixed(2)}`;
+        document.getElementById("finalTotal").value = `$ ${finalTotal.toFixed(2)}`;
+    }
 }
